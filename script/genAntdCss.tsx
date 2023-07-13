3b4cbb4db761226e9f94dac220a53e052d591b77
CheerChen <meetcheerego@gmail.com>
2023-07-13 14:03:19 +0800

    feat: add generate antd css script

---
+++ script/genAntdCss.tsx
@@ -0,0 +1,11 @@
+import fs from "fs";
+import { extractStyle } from "@ant-design/static-style-extract";
+import CustomAntdTheme from "../src/utils/customAntdTheme"
+
+const outputPath = "./static/css/antd.min.css";
+
+const css = extractStyle(CustomAntdTheme);
+
+fs.writeFileSync(outputPath, css);
+
+console.log(`🎉 Antd CSS generated at ${outputPath}`);
