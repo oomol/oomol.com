1843e1cd8aebd971e61428c61d849103ef420a7b
shaun <beingswu@gmail.com>
2024-03-04 13:10:13 +0800

    docs: add privacy docs

--- src/pages/privacy/index.tsx
+++ src/pages/privacy/index.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import Layout from "../../theme/Layout";
 import styles from "./styles.module.scss";
-import ComingSoon from "@site/src/components/ComingSoon";
+import {MDXProvider} from "@mdx-js/react";
 
+import Terms from "./privacy.mdx"
+
+const components = {
+    em(properties: any) {
+      return <i {...properties} />
+    }
+  }
 export default function Index() {
     return (
         <Layout>
-            <ComingSoon name={"Privacy"}/>
+            <MDXProvider components={components}>
+                <div className={styles.container}>
+                    <div className={styles.box}>
+                        <Terms />
+                    </div>
+                </div>
+            </MDXProvider>
         </Layout>
     );
 }
