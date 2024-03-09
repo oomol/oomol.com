3f59ca7e2a989d0d96723b98509ab2b68420b48b
shaun <beingswu@gmail.com>
2024-03-09 14:30:34 +0800

    feat(project):

--- src/pages/pricing/index.tsx
+++ src/pages/pricing/index.tsx
@@ -2,46 +2,45 @@ import React from "react";
 import Layout from "../../theme/Layout";
 import styles from "./styles.module.scss";
 import { CheckCircleFilled } from "@ant-design/icons";
+import { Button } from "antd";
+import { ButtonType } from "antd/es/button";
 
 export const PricingData = [
   {
-    type: "Hobby",
-    value: "$0",
+    meta: {
+      type: "Free",
+      sub_title: "The basics for individuals",
+      value: "$0",
+    },
     inner: [
-      "Support for 35+ Frameworks",
-      "Fast Globally (Edge Network)",
-      "Automatic CI/CD (Git Integration)",
-      "Functions (Serverless, Edge)",
-      "Starter Database (KV, Postgres)",
-      "Web Analytics",
-      "Community Support",
+      "Browse, download, and share workflows and components with the community",
+      "Store workflows in private spaces",
+      "Collaborate with others",
+      "Version history of workflows and components",
     ],
+    button: {
+      text: "Sign up for free",
+      type: "default",
+    },
   },
   {
-    type: "Pro",
-    value: "$20",
+    meta: {
+      type: "Team",
+      sub_title: "Advanced features for teams",
+      value: "$20",
+    },
     inner: [
-      "Support for 35+ Frameworks",
-      "Fast Globally (Edge Network)",
-      "Automatic CI/CD (Git Integration)",
-      "Functions (Serverless, Edge)",
-      "Starter Database (KV, Postgres)",
-      "Web Analytics",
-      "Community Support",
+      "Browse, download, and share workflows and components with the community",
+      "Store workflows in private spaces",
+      "Collaborate with team members in public spaces or private spaces",
+      "Centralized billing, on team level",
+      "Option to extend disk space for your spaces",
+      "Run and automate workflows (starting at 0.10 $ / min)",
     ],
-  },
-  {
-    type: "Enterprise",
-    value: "Custom",
-    inner: [
-      "Support for 35+ Frameworks",
-      "Fast Globally (Edge Network)",
-      "Automatic CI/CD (Git Integration)",
-      "Functions (Serverless, Edge)",
-      "Starter Database (KV, Postgres)",
-      "Web Analytics",
-      "Community Support",
-    ],
+    button: {
+      text: "Subscribe",
+      type: "primary",
+    },
   },
 ];
 
@@ -57,11 +56,32 @@ export default function Index() {
         </div>
       );
     });
+    const isTeam = data.meta.type === "Team";
     return (
-      <div className={styles.plan} key={`plan-${index}`}>
-        <div className={styles.type}>{data.type}</div>
-        <div className={styles.value}>{data.value}</div>
-        {planCell}
+      <div
+        className={styles.plan}
+        style={{
+          borderColor: isTeam ? "#7D7FE9" : "#d9d9d9",
+        }}
+        key={`plan-${index}`}
+      >
+        <div>
+          {isTeam && <div className={styles.tag}>RECOMMENDED</div>}
+          <div className={styles.meta}>
+            <div className={styles.type}>{data.meta.type}</div>
+            <div className={styles.subTitle}>{data.meta.sub_title}</div>
+            <div className={styles.price}>{data.meta.value}</div>
+            <div className={styles.month}>per user/month</div>
+          </div>
+          {planCell}
+        </div>
+        <Button
+          size="large"
+          className={styles.btn}
+          type={data.button.type as ButtonType}
+        >
+          {data.button.text}
+        </Button>
       </div>
     );
   });
