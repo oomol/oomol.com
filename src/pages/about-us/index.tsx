f9dd925af8b6a68c8efd02deaf701a385c82711e
shaun <beingswu@gmail.com>
2024-03-12 14:17:52 +0800

    feat(project): add about us page (#85)

--- src/pages/about-us/index.tsx
+++ src/pages/about-us/index.tsx
@@ -2,12 +2,33 @@ import React from "react";
 import styles from "./styles.module.scss";
 import Layout from "../../theme/Layout";
 import HomepageStarter from "@site/src/components/HomepageStarter";
-import ComingSoon from "@site/src/components/ComingSoon";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import Image from "@theme/ThemedImage";
 
 export default function Index() {
   return (
     <Layout>
-      <ComingSoon name={"About us"} />
+      <div className={styles.box}>
+        <div className={styles.about}>
+          <div className={styles["about-left"]}>
+            <div className={styles.title}>About OOMOL</div>
+            <div className={styles.description}>
+              OOMOL is a robust workflow automation platform that effortlessly
+              connects code and services through intuitive visual interactions,
+              while also allowing seamless code integration.
+            </div>
+          </div>
+          <div className={styles["about-right"]}>
+            <Image
+              style={{ width: 720 }}
+              sources={{
+                light: useBaseUrl("/img/oomol_studio.jpeg"),
+                dark: useBaseUrl("/img/oomol_studio.jpeg"),
+              }}
+            />
+          </div>
+        </div>
+      </div>
       <HomepageStarter />
     </Layout>
   );
--- src/pages/oomol-studio/index.tsx
+++ src/pages/oomol-studio/index.tsx
@@ -7,7 +7,18 @@ import ComingSoon from "@site/src/components/ComingSoon";
 export default function Index() {
   return (
     <Layout>
-      <ComingSoon name={"Oomol Studio"} />
+      <div>
+        <div>About OOMOL Studio</div>
+        <div>
+          OOMOL Studio is a robust workflow automation platform that
+          effortlessly connects code and services through intuitive visual
+          interactions, while also allowing seamless code integration.
+        </div>
+      </div>
+      <div>
+        <div>安全的容器环境</div>
+      </div>
+      <div></div>
       <HomepageStarter />
     </Layout>
   );
