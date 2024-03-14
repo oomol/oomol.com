11cc39f1ddbc371f3afe94d2c1d43fc0655b1d41
Cheerego7 <meetcheerego@gmail.com>
2024-03-14 17:24:19 +0800

    feat(pages): add useThemeMode hook to listen for changes in the global theme (#89)
    * feat(pages): add antd dark mode
    
    * feat: Added useThemeMode hook to listen for changes in the global theme
    
    * chore(library): upgrade docusaurus@3.1.1

--- src/utils/customAntdTheme/index.tsx
+++ src/utils/customAntdTheme/index.tsx
@@ -1,18 +1,34 @@
 import React from "react";
-import { ConfigProvider } from "antd";
+import { ConfigProvider, MappingAlgorithm, theme } from "antd";
+import { useThemeMode } from "../useThemeMode";
 
-const CustomAntdTheme = (children: JSX.Element) => (
-  <>
-    <ConfigProvider
-      theme={{
-        token: {
-          colorPrimary: "#7d7fe9",
-        },
-      }}
-    >
-      {children}
-    </ConfigProvider>
-  </>
-);
+const antdDarkTheme: MappingAlgorithm = (seedToken, mapToken) => ({
+  ...theme.darkAlgorithm(seedToken, mapToken),
+  colorBgLayout: "#161B22",
+});
+
+const antdLightTheme: MappingAlgorithm = seedToken => ({
+  ...theme.defaultAlgorithm(seedToken),
+  colorBgLayout: "#ecf0f7",
+});
+
+const CustomAntdTheme = (children: JSX.Element) => {
+  const themeMode = useThemeMode();
+
+  return (
+    <>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#7d7fe9",
+          },
+          algorithm: themeMode === "dark" ? antdDarkTheme : antdLightTheme,
+        }}
+      >
+        {children}
+      </ConfigProvider>
+    </>
+  );
+};
 
 export default CustomAntdTheme;
