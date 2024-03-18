567d602ae702440ddade6e9b82735aa2ff259af3
CheerChen <meetcheerego@gmail.com>
2024-03-18 16:01:34 +0800

    refactor(project): remove antd related theme configuration

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,10 +70,7 @@ const config = {
             "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: [
-            require.resolve("./static/css/antd.min.css"),
-            require.resolve("./src/css/custom.scss"),
-          ],
+          customCss: [require.resolve("./src/css/custom.scss")],
         },
       }),
     ],
