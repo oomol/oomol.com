f92f8eccbe8c9d2d7ad4024c4921142c793f53f0
shaun <beingswu@gmail.com>
2024-08-27 23:17:42 +0800

    feat(url); update url (#127)

--- src/download_url.ts
+++ src/download_url.ts
@@ -4,8 +4,8 @@ export const DownloadUrl = {
   },
   MacOS: {
     Intel:
-      "https://static.oomol.com/release/insider/darwin/x64/OomolStudio.app-1.92.2-2024-08-22.21.dmg",
+      "https://static.oomol.com/release/insider/darwin/x64/OomolStudio.app-1.92.2-2024-08-27.20.dmg",
     AppleSilicon:
-      "https://static.oomol.com/release/insider/darwin/arm64/OomolStudio.app-1.92.2-2024-08-22.21.dmg",
+      "https://static.oomol.com/release/insider/darwin/arm64/OomolStudio.app-1.92.2-2024-08-27.20.dmg",
   },
 };
