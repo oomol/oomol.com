a7327349c55805fee2b22c4952e11300feb5f637
shaun <wushuang@oomol.com>
2024-09-21 15:28:21 +0800

    update url

--- src/download_url.ts
+++ src/download_url.ts
@@ -1,11 +1,11 @@
 export const DownloadUrl = {
   Windows: {
-    X64: "https://static.oomol.com/release/insider/win32/x64/OOMOL%20Studio-1.93.1-2024-09-19.14.exe",
+    X64: "https://static.oomol.com/release/insider/win32/x64/OOMOL%20Studio-1.93.1-2024-09-20.12.exe",
   },
   MacOS: {
     Intel:
-      "https://static.oomol.com/release/insider/darwin/x64/OOMOL%20Studio.app-1.93.1-2024-09-19.22.dmg",
+      "https://static.oomol.com/release/insider/darwin/x64/OOMOL%20Studio.app-1.93.1-2024-09-20.23.dmg",
     AppleSilicon:
-      "https://static.oomol.com/release/insider/darwin/arm64/OOMOL%20Studio.app-1.93.1-2024-09-19.22.dmg",
+      "https://static.oomol.com/release/insider/darwin/arm64/OOMOL%20Studio.app-1.93.1-2024-09-20.23.dmg",
   },
 };
