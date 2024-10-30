991816fb5b932f038f0c6a2663b08ca788a117ab
shaun <wushuang@oomol.com>
2024-10-30 14:31:36 +0800

    update url

--- src/download_url.ts
+++ src/download_url.ts
@@ -6,6 +6,6 @@ export const DownloadUrl = {
     Intel:
       "https://static.oomol.com/release/insider/darwin/x64/OOMOL%20Studio.app-1.93.1-2024-09-22.23.dmg",
     AppleSilicon:
-      "https://static.oomol.com/release/insider/darwin/arm64/OOMOL%20Studio.app-1.93.1-2024-09-22.23.dmg",
+      "https://static.oomol.com/release/insider/darwin/arm64/OOMOL%20Studio.app-1.94.2-2024-10-29.23.dmg",
   },
 };
