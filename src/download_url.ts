560ad165f8f53dc3aff6efa2bc1aa254ab0a71cc
shaun <beingswu@gmail.com>
2024-12-20 23:30:09 +0800

    Update download_url.ts
    update url

--- src/download_url.ts
+++ src/download_url.ts
@@ -5,6 +5,6 @@ export const DownloadUrl = {
   MacOS: {
     Intel: "",
     AppleSilicon:
-      "https://static.oomol.com/release/stable/darwin/arm64/OOMOL%20Studio.app-1.0.0.dmg",
+      " https://static.oomol.com/release/stable/darwin/arm64/OOMOL%20Studio.app-1.0.0.dmg",
   },
 };
