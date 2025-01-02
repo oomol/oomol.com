90131280f402708d352d839f80eaf03f52271fc8
CheerChen <meetcheerego@gmail.com>
2025-01-02 11:00:54 +0800

    refactor(components): disabled docs search

--- src/theme/Navbar/index.tsx
+++ src/theme/Navbar/index.tsx
@@ -91,14 +91,14 @@ const Navbar: React.FC<NavbarProps> = memo(() => {
             <NavbarItem {...item} key={i} />
           ))}
           {/* 当路由与文档路径匹配时，显示文档搜索框 */}
-          {isDocumentPath && (
+          {/* {isDocumentPath && (
             <>
               <div className={styles.searchBar}>
                 <SearchBar />
               </div>
               <div className={styles.cutLine} />
             </>
-          )}
+          )} */}
           {/* <div className={styles.navbarBtnBox}> */}
           <Button
             className={styles["btn-nav"]}
