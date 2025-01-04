0fe4ae548736361fc38d10a39147ea9f203e45d9
shaun <beingswu@gmail.com>
2025-01-04 22:38:42 +0800

    update windows

--- src/download_url.ts
+++ src/download_url.ts
@@ -1,6 +1,6 @@
 export const DownloadUrl = {
   Windows: {
-    X64: "https://static.oomol.com/release/stable/win32/x64/OOMOL%20Studio-1.0.0-2024-12-27.11.exe",
+    X64: "https://static.oomol.com/release/stable/win32/x64/OOMOL%20Studio-1.0.0-2025-01-04.13.exe",
   },
   MacOS: {
     Intel:
