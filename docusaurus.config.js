c4867c2d74e89b52e186b4ccf442f363f61d8690
CheerChen <meetcheerego@gmail.com>
2025-01-07 22:23:01 +0800

    refactor(docusaurus): set contextualSearch config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -255,7 +255,7 @@ const config = {
         indexName: "oomol",
 
         // 可选：见下文
-        contextualSearch: false,
+        contextualSearch: true,
 
         // 可选：声明哪些域名需要用 window.location 型的导航而不是 history.push。 适用于 Algolia 配置会爬取多个文档站点，而我们想要用 window.location.href 在它们之间跳转时。
         externalUrlRegex: "external\\.com|oomol\\.com",
