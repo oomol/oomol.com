39aa192b642930829da23ae0524b33b1c3f54240
CheerChen <meetcheerego@gmail.com>
2025-01-24 21:20:45 +0800

    feat(pages): add downloads page and download button component

---
+++ src/components/DownloadButton/index.tsx
@@ -0,0 +1,97 @@
+import { DownloadUrl } from "@site/src/download_url";
+import styles from "./styles.module.scss";
+import { Popover } from "../Popover";
+import { Button } from "../Button";
+import { translate } from "@docusaurus/Translate";
+
+enum OS {
+  Windows = "Windows",
+  MacOS = "MacOS",
+}
+
+function detectOSAndArchitecture(): OS {
+  const userAgent = navigator.userAgent;
+  let os = OS.Windows;
+
+  if (userAgent.indexOf("Win") !== -1) {
+    os = OS.Windows;
+  } else if (userAgent.indexOf("Mac") !== -1) {
+    os = OS.MacOS;
+  }
+
+  return os;
+}
+
+const content = (
+  <div className={styles.popoverBox}>
+    <a download href={DownloadUrl.Stable.MacOS.AppleSilicon}>
+      <div className={styles.popoverBtn}>
+        <div className={`${styles.icon} i-ic-baseline-apple`} />
+        <span style={{ marginLeft: 8 }}>Apple Silicon</span>
+      </div>
+    </a>
+    <a download href={DownloadUrl.Stable.MacOS.Intel}>
+      <div className={styles.popoverBtn}>
+        <div className={`${styles.icon} i-file-icons-intel`} />
+        <span style={{ marginLeft: 8 }}>Intel Chip</span>
+      </div>
+    </a>
+  </div>
+);
+
+export interface DownloadButtonProps {
+  stableTag?: boolean;
+}
+
+export const DownloadButton = ({ stableTag }: DownloadButtonProps) => {
+  return (
+    <div className={styles["button-box"]}>
+      {detectOSAndArchitecture() === OS.MacOS ? (
+        <Popover
+          trigger={
+            <Button
+              className={styles.download}
+              icon={
+                <div
+                  className="i-codicon-desktop-download"
+                  style={{ fontSize: 18 }}
+                />
+              }
+            >
+              {translate({
+                message: stableTag
+                  ? "HOME.FirstScreen.download-macos"
+                  : "HOME.FirstScreen.download-macos-stable",
+              })}
+            </Button>
+          }
+          content={content}
+        />
+      ) : (
+        <div className={styles.windowsBox}>
+          <Button
+            className={styles.download}
+            href={DownloadUrl.Stable.Windows.x64}
+            icon={
+              <div
+                className="i-codicon-desktop-download"
+                style={{ fontSize: 18 }}
+              />
+            }
+          >
+            {translate({
+              message: stableTag
+                ? "HOME.FirstScreen.download-windows"
+                : "HOME.FirstScreen.download-windows-stable",
+            })}
+          </Button>
+          <span className={styles.windowsSubtitle}>
+            {translate({
+              message: "HOME.FirstScreen.download-windows-subtitle",
+            })}
+          </span>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ src/pages/downloads/index.tsx
@@ -0,0 +1,135 @@
+import styles from "./styles.module.scss";
+
+import React from "react";
+import Layout from "@theme/Layout";
+import { DownloadButton } from "@site/src/components/DownloadButton";
+import { DownloadUrl } from "@site/src/download_url";
+import { translate } from "@docusaurus/Translate";
+
+const downloadData = [
+  {
+    icon: "i-codicon-verified-filled",
+    type: translate({
+      message: "HOME.Downloads.Stable.title",
+    }),
+    subTitle: translate({
+      message: "HOME.Downloads.Stable.subtitle",
+    }),
+    downloads: [
+      {
+        text: "Download for Windows x64",
+        url: DownloadUrl.Stable.Windows.x64,
+      },
+      {
+        text: "Download for macOS Apple Silicon",
+        url: DownloadUrl.Stable.MacOS.AppleSilicon,
+      },
+      {
+        text: "Download for macOS Intel Chip",
+        url: DownloadUrl.Stable.MacOS.Intel,
+      },
+    ],
+    className: styles.stable,
+  },
+  {
+    icon: "./img/canary.svg",
+    type: translate({
+      message: "HOME.Downloads.Canary.title",
+    }),
+    subTitle: translate({
+      message: "HOME.Downloads.Canary.subtitle",
+    }),
+    downloads: [
+      {
+        text: "Download for Windows x64",
+        url: DownloadUrl.Canary.Windows.x64,
+      },
+      {
+        text: "Download for macOS Apple Silicon",
+        url: DownloadUrl.Canary.MacOS.AppleSilicon,
+      },
+      {
+        text: "Download for macOS Intel Chip",
+        url: DownloadUrl.Canary.MacOS.Intel,
+      },
+    ],
+  },
+  {
+    icon: "i-codicon-color-mode",
+    type: translate({
+      message: "HOME.Downloads.Nightly.title",
+    }),
+    subTitle: translate({
+      message: "HOME.Downloads.Nightly.subtitle",
+    }),
+    downloads: [
+      {
+        text: "Download for Windows x64",
+        url: DownloadUrl.Nightly.Windows.x64,
+      },
+      {
+        text: "Download for macOS Apple Silicon",
+        url: DownloadUrl.Nightly.MacOS.AppleSilicon,
+      },
+      {
+        text: "Download for macOS Intel Chip",
+        url: DownloadUrl.Nightly.MacOS.Intel,
+      },
+    ],
+  },
+];
+
+export default function Downloads() {
+  return (
+    <Layout title="Hello" description="Hello React Page">
+      <div className={styles.container}>
+        <img className={styles.image} src={"/img/logo2x.png"} />
+        <h1 className={styles.title}>
+          {translate({
+            message: "HOME.Downloads.title",
+          })}
+        </h1>
+        <p className={styles["sub-title"]}>
+          {translate({
+            message: "HOME.Downloads.subtitle",
+          })}
+        </p>
+        <div className={styles["download-button"]}>
+          <DownloadButton />
+        </div>
+        <div className={styles.cards}>
+          {downloadData.map((item, index) => {
+            return (
+              <div
+                className={`${styles.card} ${item.className}`}
+                key={`download-${index}`}
+              >
+                <div className={styles.content}>
+                  {item.type === "Canary" ? (
+                    <img className={styles["icon-canary"]} src={item.icon} />
+                  ) : (
+                    <i className={`${styles.icon} ${item.icon}`} />
+                  )}
+                  <p className={styles.type}>{item.type}</p>
+                  <p className={styles.description}>{item.subTitle}</p>
+                </div>
+                <div className={styles.downloads}>
+                  {item.downloads.map((download, i) => {
+                    return (
+                      <a key={`download-${index}-${i}`} href={download.url}>
+                        <i
+                          className={`${styles["download-icon"]} i-codicon-desktop-download`}
+                        />
+                        {download.text}
+                      </a>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Layout>
+  );
+}
