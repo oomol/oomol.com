4c0c4857d1f2dbbd2183b31dd58db6e1455bec01
Cheerego7 <meetcheerego@gmail.com>
2025-05-30 10:38:57 +0800

    fix(pages): improve mobile styles (#207)

--- src/components/HomePageBuiltInLLM/index.tsx
+++ src/components/HomePageBuiltInLLM/index.tsx
@@ -4,18 +4,18 @@ import ClaudeSVG from "@site/static/img/pages/home/claude.svg";
 import ClaudeLightHighSVG from "@site/static/img/pages/home/claude-highlight.svg";
 import OpenAISVG from "@site/static/img/pages/home/openai.svg";
 import OpenAILightHighSVG from "@site/static/img/pages/home/openai-highlight.svg";
-import CopilotSVG from "@site/static/img/pages/home/copilot.svg";
-import CopilotLightHighSVG from "@site/static/img/pages/home/copilot-highlight.svg";
 import DoubaoSVG from "@site/static/img/pages/home/doubao.svg";
 import DoubaoLightHighSVG from "@site/static/img/pages/home/doubao-highlight.svg";
 import GrokSVG from "@site/static/img/pages/home/grok.svg";
 import GrokLightHighSVG from "@site/static/img/pages/home/grok-highlight.svg";
+import GeminiSVG from "@site/static/img/pages/home/gemini.svg";
+import GeminiLightHighSVG from "@site/static/img/pages/home/gemini-highlight.svg";
 import MistralAISVG from "@site/static/img/pages/home/mistralai.svg";
 import MistralAILightHighSVG from "@site/static/img/pages/home/mistralai-highlight.svg";
 import QwenSVG from "@site/static/img/pages/home/qwen.svg";
 import QwenLightHighSVG from "@site/static/img/pages/home/qwen-highlight.svg";
-import SiliconCloudSVG from "@site/static/img/pages/home/siliconcloud.svg";
-import SiliconCloudLightHighSVG from "@site/static/img/pages/home/siliconcloud-highlight.svg";
+import DeepSeekSVG from "@site/static/img/pages/home/deepseek.svg";
+import DeepSeekLightHighSVG from "@site/static/img/pages/home/deepseek-highlight.svg";
 
 import { memo } from "react";
 import { AuroraText } from "../magic-ui/AuroraText";
@@ -30,13 +30,21 @@ const llmData = [
     defaultIcon: <OpenAISVG />,
     hoverIcon: <OpenAILightHighSVG />,
   },
-  // {
-  //   defaultIcon: <CopilotSVG />,
-  //   hoverIcon: <CopilotLightHighSVG />,
-  // },
+  {
+    defaultIcon: <GeminiSVG />,
+    hoverIcon: <GeminiLightHighSVG />,
+  },
   {
     defaultIcon: <DoubaoSVG />,
     hoverIcon: <DoubaoLightHighSVG />,
+  },
+  {
+    defaultIcon: <DeepSeekSVG />,
+    hoverIcon: <DeepSeekLightHighSVG />,
+  },
+  {
+    defaultIcon: <QwenSVG />,
+    hoverIcon: <QwenLightHighSVG />,
   },
   {
     defaultIcon: <GrokSVG />,
@@ -46,14 +54,6 @@ const llmData = [
     defaultIcon: <MistralAISVG />,
     hoverIcon: <MistralAILightHighSVG />,
   },
-  {
-    defaultIcon: <QwenSVG />,
-    hoverIcon: <QwenLightHighSVG />,
-  },
-  // {
-  //   defaultIcon: <SiliconCloudSVG />,
-  //   hoverIcon: <SiliconCloudLightHighSVG />,
-  // },
 ];
 
 export const LLMItem = memo(
