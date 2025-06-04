4867441afe83bb7970118d147dd622ea7fe518c9
Kalopsia-42 <stardust3906@gmail.com>
2025-06-04 11:24:35 +0800

    refactor(pages): update community page

--- src/components/Button/LinkBtn.tsx
+++ src/components/Button/LinkBtn.tsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { ReactNode } from "react";
 import styles from "./LinkBtn.module.scss";
 import clsx from "clsx";
 
 export interface LinkBtnProps {
   text: string;
   iconPos: "left" | "right";
-  icon: string;
-  url: string;
+  icon: ReactNode | string;
+  url?: string;
   className?: string;
 }
 export default function LinkBtn({
@@ -16,18 +16,21 @@ export default function LinkBtn({
   url,
   className,
 }: LinkBtnProps) {
+  const handleClick = () => {
+    if (url) {
+      window.open(url, "_blank");
+    }
+  };
   return (
-    <button
-      // className={styles.container}
-      className={clsx(styles.container, className)}
-      onClick={() => window.open(url, "_blank")}
-    >
+    <button className={clsx(styles.container, className)} onClick={handleClick}>
       <div
         className={`${styles.linkBtn}
         ${iconPos === "left" ? styles.left : styles.right}`}
       >
         <div className={styles.linkText}>{text}</div>
-        <i className={`${styles.linkIcon} ${icon}`} />
+        <span className={styles.linkIcon}>
+          {typeof icon === "string" ? <i className={`${icon}`} /> : icon}
+        </span>
       </div>
     </button>
   );
