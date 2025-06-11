8ccbe9ff7c050aa446b703c05c9cb832269d4ea7
l1shen <lishen1635@gmail.com>
2025-06-11 13:27:40 +0800

    fix: move redirect-lang.js to innerHTML

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,24 +46,64 @@ const config = {
         blogSidebarTitle: "Updates",
       },
     ],
-    // function () {
-    //   return {
-    //     name: "docusaurus-plugin-inject-html",
-    //     injectHtmlTags() {
-    //       return {
-    //         headTags: [
-    //           {
-    //             tagName: "script",
-    //             attributes: {
-    //               charset: "utf-8",
-    //               src: "./scripts/redirect-lang.js",
-    //             },
-    //           },
-    //         ],
-    //       };
-    //     },
-    //   };
-    // },
+    function () {
+      return {
+        name: "docusaurus-plugin-inject-html",
+        injectHtmlTags() {
+          return {
+            headTags: [
+              {
+                tagName: "script",
+                innerHTML: `
+  (() => {
+  // not in browser
+  if (typeof window === "undefined") return;
+
+  function redirect() {
+    const rootPath = "/";
+    const matchRouteRoot = location.pathname === rootPath;
+
+    const localLocale = localStorage.getItem("locale");
+
+    if (!localLocale) {
+      const isZhCN = navigator.language.includes("zh-");
+
+      if (matchRouteRoot) {
+        if (isZhCN) {
+          location.pathname = location.pathname.replace("/", "/zh-CN/");
+          localStorage.setItem("locale", "zh-CN");
+          return;
+        }
+
+        localStorage.setItem("locale", "en");
+      }
+    }
+
+    if (matchRouteRoot) {
+      // 如果当前 localStorage 的语言为英文, 且当前路径为中文首页，则跳转到英文首页 "/"
+      if (localLocale === "en" && location.pathname.startsWith("/zh-CN/")) {
+        location.pathname = location.pathname.replace("/zh-CN/", "/");
+        return;
+      }
+
+      //如果当前 localStorage 的语言为中文, 且当前路径为英文首页，则跳转到中文首页 "/zh-CN/"
+      if (localLocale === "zh-CN" && !location.pathname.startsWith("/zh-CN/")) {
+        location.pathname = location.pathname.replace("/", "/zh-CN/");
+        return;
+      }
+    }
+  }
+
+  redirect();
+})();
+`,
+              },
+              ,
+            ],
+          };
+        },
+      };
+    },
     function () {
       return {
         name: "unocss",
