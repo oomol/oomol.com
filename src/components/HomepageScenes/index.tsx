80d5a41126a1907f172fe875fa552193575d40ad
CheerChen <meetcheerego@gmail.com>
2025-09-11 18:52:10 +0800

    refactor(pages): improve the grid layout

--- src/components/HomepageScenes/index.tsx
+++ src/components/HomepageScenes/index.tsx
@@ -3,8 +3,8 @@ import styles from "./styles.module.scss";
 import Image from "@theme/ThemedImage";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 import { translate } from "@docusaurus/Translate";
-import { BlurFade } from "../magic-ui/BlurFade";
 import LinkBtn from "../Button/LinkBtn";
+import clsx from "clsx";
 
 type ScenesDataType = {
   imageUrl: string;
@@ -64,56 +64,57 @@ const scenesData: ScenesDataType[] = [
     inner: translate({ message: "HOME.Scenes.automation.inner" }),
     icon: "i-codicon-gear",
   },
-  {
-    imageUrl: "/img/scenes/web-scraping.svg",
-    title: translate({ message: "HOME.Scenes.web-scraping.title" }),
-    type: "purple",
-    color: "orange",
-    inner: translate({ message: "HOME.Scenes.web-scraping.inner" }),
-    icon: "i-codicon-globe",
-  },
-  {
-    imageUrl: "/img/scenes/api-integration.svg",
-    title: translate({ message: "HOME.Scenes.api-integration.title" }),
-    type: "purple",
-    color: "indigo",
-    inner: translate({ message: "HOME.Scenes.api-integration.inner" }),
-    icon: "i-codicon-plug",
-  },
-  {
-    imageUrl: "/img/scenes/content-generation.svg",
-    title: translate({ message: "HOME.Scenes.content-generation.title" }),
-    type: "purple",
-    color: "yellow",
-    inner: translate({ message: "HOME.Scenes.content-generation.inner" }),
-    icon: "i-codicon-wand",
-  },
+  // {
+  //   imageUrl: "/img/scenes/web-scraping.svg",
+  //   title: translate({ message: "HOME.Scenes.web-scraping.title" }),
+  //   type: "purple",
+  //   color: "orange",
+  //   inner: translate({ message: "HOME.Scenes.web-scraping.inner" }),
+  //   icon: "i-codicon-globe",
+  // },
+  // {
+  //   imageUrl: "/img/scenes/api-integration.svg",
+  //   title: translate({ message: "HOME.Scenes.api-integration.title" }),
+  //   type: "purple",
+  //   color: "indigo",
+  //   inner: translate({ message: "HOME.Scenes.api-integration.inner" }),
+  //   icon: "i-codicon-plug",
+  // },
+  // {
+  //   imageUrl: "/img/scenes/content-generation.svg",
+  //   title: translate({ message: "HOME.Scenes.content-generation.title" }),
+  //   type: "purple",
+  //   color: "yellow",
+  //   inner: translate({ message: "HOME.Scenes.content-generation.inner" }),
+  //   icon: "i-codicon-wand",
+  // },
 ];
 
 export default function HomepageScenes() {
   const scenesNodes = scenesData.map((data, index) => {
     return (
-      <BlurFade key={`scenes-${index}`} delay={0.1 * index}>
-        <div className={styles.gridItem}>
-          <div className={styles.scenesCard}>
-            <Image
-              className={styles.scenesCardImage}
-              sources={{
-                light: useBaseUrl(data.imageUrl),
-                dark: useBaseUrl(data.imageUrl),
-              }}
-            />
-            <div className={styles.scenesCardContent}>
-
-              <div className={styles.scenesTextTitle}>
-                <i className={data.icon} />
-                {data.title}
-              </div>
-              <p className={styles.scenesTextInner}>{data.inner}</p>
+      <div
+        className={clsx(styles.gridItem, {
+          [styles.gridFirstItem]: index === 0,
+        })}
+      >
+        <div className={styles.scenesCard}>
+          <Image
+            className={styles.scenesCardImage}
+            sources={{
+              light: useBaseUrl(data.imageUrl),
+              dark: useBaseUrl(data.imageUrl),
+            }}
+          />
+          <div className={styles.scenesCardContent}>
+            <div className={styles.scenesTextTitle}>
+              <i className={data.icon} />
+              {data.title}
             </div>
+            <p className={styles.scenesTextInner}>{data.inner}</p>
           </div>
         </div>
-      </BlurFade>
+      </div>
     );
   });
   return (
@@ -129,9 +130,7 @@ export default function HomepageScenes() {
             message: "HOME.Scenes.subtitle",
           })}
         </span>
-        <div className={styles.gridContainer}>
-          {scenesNodes}
-        </div>
+        <div className={styles.gridContainer}>{scenesNodes}</div>
         <div className={styles.linkButtonContainer}>
           <LinkBtn
             text={translate({ message: "HOME.Scenes.link-button" })}
